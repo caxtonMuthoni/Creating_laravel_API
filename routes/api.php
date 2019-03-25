759ce9c61de444bb5534281a6988e2ab1fbbd16d
caxtonMuthoni <githinjicaxton323@gmail.com>
2019-03-25 12:33:55 +0300

    created model -a

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('/products','ProductsController');
+Route::group(['prefix=>products'],function(){
+Route::apiResource('/{product}/review','ReviewController');
+});
